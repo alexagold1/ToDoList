15f00a32692b23547863a87c494d9829fd8a761f
alexagold1 <526agold@frhsd.com>
2025-02-25 09:29:07 -0500

    first commit

---
+++ script.js
@@ -0,0 +1,11 @@
+//Init(Create) an empty array element called tasks
+let tasks = []
+
+//Add an ON CLICK event listener to the "add task button" that calls a function
+document.getElementById('addTaskBtn').addEventListener('click', function(){ 
+    //get the value of the input box and store it in a variable called taskInput
+    let taskInput = document.getElementById('taskInput').value
+
+    //Check if taskInput has value or something in it
+    if(taskInput)
+})
